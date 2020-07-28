8612cb0ff7d8dca8957af66d93726c67329dd103
MrB <787263646@qq.com>
2020-07-28 14:21:00 +0800

    坦克越界

--- ui/action.py
+++ ui/action.py
@@ -4,13 +4,32 @@ from abc import *
 """
 1. 导入模块
 2. 类中metaclass=ABCMeta
-3. @abstractclassmethod修饰方法
+3. @abstractmethod修饰方法
 """
 class Display(metaclass=ABCMeta):
     """
     抽象类：规范显示行为
     """
-    @abstractclassmethod
+    @abstractmethod
     def display(self):
         """显示"""
+        pass
+
+class Move(metaclass=ABCMeta):
+    """移动的规范"""
+    @abstractmethod
+    def move(self, direction):
+        pass
+    @abstractmethod
+    def is_blocked(self, block):
+        pass
+
+class Block(metaclass=ABCMeta):
+    """阻塞的规范"""
+    pass
+
+class Order(metaclass=ABCMeta):
+    """排序显示"""
+    @abstractmethod
+    def get_order(self):
         pass
--- ui/view.py
+++ ui/view.py
@@ -3,7 +3,7 @@ from ui.locals import *
 from ui.action import *
 
 
-class PlayerTank(Display):
+class PlayerTank(Display, Move):
 
     def __init__(self, **kwargs):
         self.x = kwargs["x"]
@@ -18,7 +18,13 @@ class PlayerTank(Display):
         # surface
         self.surface = kwargs["surface"]
         # speed
-        self.speed = 5
+        self.speed = 1
+        # 错误的方向
+        self.bad_direction = Direction.NONE
+        # width , height
+        self.width = self.images[0].get_width()
+        self.height = self.images[0].get_height()
+
 
     def display(self):
         image = None
@@ -39,6 +45,9 @@ class PlayerTank(Display):
         # 如果当前的方向和要去走的方向是不同的
         # 1. 只是转方向
         # 2. 转方向+移动
+        # 如果是错误的方向就不移动了
+        if direction == self.bad_direction:
+            return
 
         if self.direction != direction:
             # 1. 只是转方向
@@ -48,19 +57,56 @@ class PlayerTank(Display):
             # 方向相同
             if direction == Direction.UP:
                 self.y -= self.speed
+                if self.y < 0:
+                    self.y = 0
             elif direction == Direction.DOWN:
                 self.y += self.speed
+                if self.y > GAME_HEIGHT - self.height:
+                    self.y = GAME_HEIGHT - self.height
             elif direction == Direction.LEFT:
                 self.x -= self.speed
+                if self.x < 0:
+                    self.x = 0
             elif direction == Direction.RIGHT:
                 self.x += self.speed
+                if self.x > GAME_WIDTH - self.width:
+                    self.x = GAME_WIDTH - self.width
 
     def fire(self):
         # TODO:
         pass
+    def is_blocked(self, wall):
+        # 判断坦克和墙碰撞
+
+        # 判断坦克下一步的矩形和现在的墙是否碰撞
+        next_x = self.x
+        next_y = self.y
+
+        if self.direction == Direction.UP:
+            next_y -= self.speed
+        elif self.direction == Direction.DOWN:
+            next_y += self.speed
+        elif self.direction == Direction.LEFT:
+            next_x -= self.speed
+        elif self.direction == Direction.RIGHT:
+            next_x += self.speed
+
+        # 矩形和矩形的碰撞， 当前矩形
+        rect_self = pygame.Rect(next_x, next_y, self.width, self.height)
+        rect_wall = pygame.Rect(wall.x, wall.y, wall.width, wall.height)
+
+        collide = pygame.Rect.colliderect(rect_self, rect_wall)
+        if collide:
+            # 碰撞了,当前的方向是错误的方向
+            self.bad_direction = self.direction
+            return True
+        else:
+            # 没有错误方向
+            self.bad_direction = Direction.NONE
+            return False
 
 
-class wall(Display):
+class WaLL(Display, Block):
     """砖墙"""
 
     def __init__(self, **kwargs):
@@ -68,6 +114,9 @@ class wall(Display):
         self.y = kwargs["y"]
         self.image = pygame.image.load("img/walls.gif")
         self.surface = kwargs["surface"]
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
 
     def display(self):
         self.surface.blit(self.image, (self.x, self.y))
@@ -76,7 +125,7 @@ class wall(Display):
         pass
 
 
-class Steel(Display):
+class Steel(Display, Block):
     """铁墙"""
 
     def __init__(self, **kwargs):
@@ -84,6 +133,39 @@ class Steel(Display):
         self.y = kwargs["y"]
         self.image = pygame.image.load("img/steels.gif")
         self.surface = kwargs["surface"]
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
     def display(self):
         self.surface.blit(self.image, (self.x, self.y))
+
+class Water(Display):
+    """水"""
+
+    def __init__(self, **kwargs):
+        self.x = kwargs["x"]
+        self.y = kwargs["y"]
+        self.image = pygame.image.load("img/water.gif")
+        self.surface = kwargs["surface"]
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+    def display(self):
+        self.surface.blit(self.image, (self.x, self.y))
+
+class Grass(Display, Order):
+    """丛林"""
+
+    def __init__(self, **kwargs):
+        self.x = kwargs["x"]
+        self.y = kwargs["y"]
+        self.image = pygame.image.load("img/grass.png")
+        self.surface = kwargs["surface"]
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+    def display(self):
+        self.surface.blit(self.image, (self.x, self.y))
+
+    def get_order(self):
+        return 100
